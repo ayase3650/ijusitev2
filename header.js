9150b4c928d34ccf9a33e7fde67ba726bbd00feb
ayase3650 <ayase3650@gmail.com>
2026-07-24 11:53:52 +0900

    Add files via upload

---
+++ header.js
@@ -0,0 +1,19 @@
+document.write(`
+    <div class="top-alert-bar">
+        <span> 移住の準備・手続きはこちら！</span>
+        <a href="index.html#consult" class="btn-banner">診断ナビ</a>
+        <a href="application.html" class="btn-banner">各種申請</a>
+    </div>
+
+    <header>
+        <h1 class="site-title"><a href="index.html" style="color:var(--fugu-blue);">SHIMONOSEKI LIFE</a></h1>
+        <nav>
+            <ul>
+                <li><a href="index.html#support" class="puku-hover">SUPPORT</a></li>
+                <li><a href="index.html#faq" class="puku-hover">FAQ</a></li>
+                <li><a href="application.html">各種申請</a></li>
+                <li><a href="index.html#consult" class="puku-hover" style="color: var(--fugu-fin);"><img src="2.png" alt="せきまる" class="seki-icon seki-sway">診断ナビ</a></li>
+            </ul>
+        </nav>
+    </header>
+`);
